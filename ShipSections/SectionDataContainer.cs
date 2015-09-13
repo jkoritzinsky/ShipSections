01bbd2ce78acbcf895826ad1fd28c523b2db8134
Jeremy Koritzinsky <jkoritzinsky@live.com>
2015-09-13 01:31:40 -0500

    Added support for adding data to (in-code) section data storage.  Stores by mod and then by section.

--- ShipSections/SectionDataContainer.cs
+++ ShipSections/SectionDataContainer.cs
@@ -9,8 +9,17 @@ namespace JKorTech.ShipSections
     public class SectionDataContainer : ConfigNodeStorage
     {
         [Persistent]
-        public Dictionary<string, SectionData> data = new Dictionary<string, SectionData>();
+        public Dictionary<string, List<SectionData>> data = new Dictionary<string, List<SectionData>>();
 
-        public IEnumerable<SectionData> GetSectionDataForMod(string modID) => data.Where(pair => pair.Key == modID).Select(pair => pair.Value);
+        public IEnumerable<SectionData> GetSectionDataForMod(string modID) => data.Where(pair => pair.Key == modID).SelectMany(pair => pair.Value);
+
+        public void AddOrUpdateSectionDataForMod(string modID, SectionData sectionData)
+        {
+            if(data.Any(pair => pair.Key == modID && pair.Value.Any(section => section.sectionName == sectionData.sectionName)))
+            {
+                data[modID].RemoveAll(section => section.sectionName == sectionData.sectionName);
+                data[modID].Add(sectionData);
+            }
+        }
     }
 }
