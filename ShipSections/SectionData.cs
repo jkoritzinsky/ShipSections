804739266bf8df5ebfb5eb39c06e548673b67e46
Jeremy Koritzinsky <jkoritzinsky@live.com>
2015-09-13 01:23:00 -0500

    Started development of section data persistence support for mods that use ShipSections.

---
+++ ShipSections/SectionData.cs
@@ -0,0 +1,10 @@
+﻿using KSPPluginFramework;
+
+namespace JKorTech.ShipSections
+{
+    public abstract class SectionData : ConfigNodeStorage
+    {
+        [Persistent]
+        public string sectionName = string.Empty;
+    }
+}
---
+++ ShipSections/SectionDataContainer.cs
@@ -0,0 +1,16 @@
+﻿using KSPPluginFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JKorTech.ShipSections
+{
+    public class SectionDataContainer : ConfigNodeStorage
+    {
+        [Persistent]
+        public Dictionary<string, SectionData> data = new Dictionary<string, SectionData>();
+
+        public IEnumerable<SectionData> GetSectionDataForMod(string modID) => data.Where(pair => pair.Key == modID).Select(pair => pair.Value);
+    }
+}
